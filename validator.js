c652bad621b657ee7e314b151a9cfbc7ca9f0530
Matt DeGoey <degoeym@gmail.com>
2014-10-23 19:31:19 +0000

    Redid with ternary operator because I can and I was bored.

--- validator.js
+++ validator.js
@@ -1,3 +1,11 @@
+function Show(position) {
+    
+}
+
+function Hide() {
+    
+}
+
 $(function(){
 	$('li').hide();
 
@@ -5,28 +13,9 @@ $(function(){
 		var email = $('input[type=text]').val().toString();
 		var password = $('input[type=password]').val().toString();
 		
-		if (!email.match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/)) {
-			$('ul li:nth-child(1)').slideDown('slow');
-		} else {
-			$('ul li:nth-child(1)').slideUp('slow');
-		}
-
-		if (password.length < 8) {
-			$('ul li:nth-child(2)').slideDown('slow');
-		} else {
-			$('ul li:nth-child(2)').slideUp('slow');
-		}
-
-		if (!password.match(/^(?=.*[A-Z]).+$/)) {
-			$('ul li:nth-child(3)').slideDown('slow');
-		} else {
-			$('ul li:nth-child(3)').slideUp('slow');
-		}
-
-		if (!password.match(/.*[0-9].*/)) {
-			$('ul li:nth-child(4)').slideDown('slow');
-		} else {
-			$('ul li:nth-child(4)').slideUp('slow');
-		}
+		var isEmail = !email.match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/) ? $('ul li:nth-child(1)').slideDown('slow') : $('ul li:nth-child(1)').slideUp('slow'); 
+        var passwordLength = password.length < 8 ? $('ul li:nth-child(2)').slideDown('slow') : $('ul li:nth-child(2)').slideUp('slow');
+		var hasCapLetter = !password.match(/^(?=.*[A-Z]).+$/) ? $('ul li:nth-child(3)').slideDown('slow') : $('ul li:nth-child(3)').slideUp('slow');
+		var hasNumber = !password.match(/.*[0-9].*/) ? $('ul li:nth-child(4)').slideDown('slow') : $('ul li:nth-child(4)').slideUp('slow');
 	});
 });
