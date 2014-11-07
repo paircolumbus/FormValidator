845afdf76927c14e1a0622497e2ae547f21376c5
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2014-11-07 09:40:20 -0500

    rearranged code a bit

--- validator.js
+++ validator.js
@@ -5,38 +5,35 @@
   	$('.errors > li').hide();
   });
 
-  function validateForm() {
-  	$('.errors > li').hide();
-  	if ($('input:password').val() == "" && $('input:text').val() == ""){
-  		return false;
-  	}
-  	validateEmail($('input:text').val());
-  	validatePassword($('input:password').val());
+  var Validate = { 
+    form: function() {
+     $('.errors > li').hide();
+     if ($('input:password').val() == "" && $('input:text').val() == ""){
+      return false;
+    }
+    Validate.email($('input:text').val());
+    Validate.password($('input:password').val());
 
-  	if ($('.errors > li').is(':visible')){
-  		return false;
-  	}
-  	return true;
+    if ($('.errors > li').is(':visible')){
+      return false;
+    }
+    return true;
+  },
+    email: function (email) { 
+     var emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+     if (!emailFormat.test(email)) {
+      Validate.showEmailErrors();
+    }
+  }, 
+  password: function(password){
+   if (!password.length >= 8 || !/[A-Z]/.test(password) || !/[0-9]/.test(password)){
+    Validate.showPasswordErrors();
   }
-
-  function validateEmail(email) { 
-  	var emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  	if (!emailFormat.test(email)) {
-  		showEmailErrors();
-  	}
-  } 
-
-  function validatePassword(password){
-  	if (!password.length >= 8 || !/[A-Z]/.test(password) || !/[0-9]/.test(password)){
-  		showPasswordErrors();
-  	}
+  },
+  showPasswordErrors: function(){
+   $(".errors > li:contains('password')").show();
+  },
+  showEmailErrors: function() {
+   $(".errors > li:contains('email')").show();
   }
-
-  function showPasswordErrors(){
-  	$(".errors > li:contains('password')").show();
-  }
-
-  function showEmailErrors() {
-  	$(".errors > li:contains('email')").show();
-  }
-
+}
