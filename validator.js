a2cbc2de34dfd808c027fd87ed3b37ad0b1036db
Jonathan Arnett <JArnett@covermymeds.com>
2015-06-01 14:13:05 -0400

    J3RN's solution

--- validator.js
+++ validator.js
@@ -1,7 +1,30 @@
-
-  //insert your code here
- 
 $(function(){
+    $("li").hide();
 
-  //insert your code here
+    $("form").submit(function(event) {
+        $("li").hide();
+
+        var email =     $($("input")[0]).val();
+        var password =  $($("input")[1]).val();
+
+        if (!email.match(/[\w\.]+@[\w\.]+\.[\w\.]+/)) {
+            $($("li")[0]).show();
+            event.preventDefault();
+        }
+
+        if (password.length < 8) {
+            $($("li")[1]).show();
+            event.preventDefault();
+        }
+
+        if (!password.match(/[A-Z]/)) {
+            $($("li")[2]).show();
+            event.preventDefault();
+        }
+
+        if (!password.match(/\d/)) {
+            $($("li")[3]).show();
+            event.preventDefault();
+        }
+    });
 });
