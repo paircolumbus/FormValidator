d79fbe1b98383446c3ef2b0667316701e23c68b3
Kaleb Davis <kbd6024@rit.edu>
2015-06-08 17:04:36 -0400

    Continue work on coffeescript, not working

--- validator.js
+++ validator.js
@@ -2,38 +2,6 @@
 (function() {
   var check_email, check_is_capital, check_is_number, check_long_enough;
 
-  $(".errors li").hide;
-
-  (function() {
-    return $("form").submit = function(event) {
-      var email, is_valid, password;
-      is_valid = false;
-      $(".errors li").hide;
-      email = $("input[placeholder='Email']").val;
-      password = $("input[placeholder='Password']").val;
-      if (!check_email(email)) {
-        is_valid = false;
-        $(".errors li:nth-child(1)").show;
-      }
-      if (!check_long_enough(password)) {
-        is_valid = false;
-        $(".errors li:nth-child(2)").show;
-      }
-      if (!check_is_capital(password)) {
-        is_valid = false;
-        $(".errors li:nth-child(3)").show;
-      }
-      if (!check_is_number(password)) {
-        is_valid = false;
-        $(".errors li:nth-child(4)").show;
-      }
-      if (!is_valid) {
-        event.stopPropagation;
-        return event.preventDefault;
-      }
-    };
-  });
-
   check_email = function(email) {
     return /[a-zA-Z0-9-._]+@[a-zA-Z0-9-._]+\.[a-zA-Z0-9-._]/.test(email);
   };
@@ -49,5 +17,39 @@
   check_is_capital = function(password) {
     return /[A-Z]/.test(password);
   };
+
+  $(document).on('ready page:load', function() {
+    $('.errors li').hide();
+  });
+
+  $(function() {
+    $("form").submit = function(event) {
+      var email, is_valid, password;
+      is_valid = false;
+      $(".errors li").hide;
+      email = $("input[placeholder='Email']").val;
+      password = $("input[placeholder='Password']").val;
+      if (!check_email(email)) {
+        is_valid = false;
+        $(".errors li:nth-child(1)").show();
+      }
+      if (!check_long_enough(password)) {
+        is_valid = false;
+        $(".errors li:nth-child(2)").show();
+      }
+      if (!check_is_capital(password)) {
+        is_valid = false;
+        $(".errors li:nth-child(3)").show();
+      }
+      if (!check_is_number(password)) {
+        is_valid = false;
+        $(".errors li:nth-child(4)").show();
+      }
+      if (!is_valid) {
+        event.stopPropagation;
+        event.preventDefault;
+      }
+    };
+  });
 
 }).call(this);
