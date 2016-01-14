de71e5641be4adbbf36f83092b4f7a90dfbce1c0
Priya <priya.ramesh6@gmail.com>
2016-01-14 13:28:47 -0500

    indentation wasn't uniform

--- validator.js
+++ validator.js
@@ -4,34 +4,34 @@ $(function(){
 
 	$("input:submit").click(function () {
                    
-             var entered_email = $("input:text").val();
-             alert(entered_email);
+            var entered_email = $("input:text").val();
+            alert(entered_email);
                    
-             var entered_password = $("input:password").val();
-             alert(entered_password);
+            var entered_password = $("input:password").val();
+            alert(entered_password);
                   
-    			   var email = new RegExp(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/);
-    			   var pswd =  new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/);
+    		var email = new RegExp(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/);
+    		var pswd =  new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/);
 
-             var res =  email.test(entered_email);
-             var res_pswd =  pswd.test(entered_password);
+            var res =  email.test(entered_email);
+            var res_pswd =  pswd.test(entered_password);
   				   
-  				  if (res == true && res_pswd == true) {
+  			if (res == true && res_pswd == true) {
           			alert('Congrats! thats a valid email address and password');
-         			  $(".errors").hide();
-         		}
-				    if (res == true && res_pswd != true){
-				   		  alert('Invalid password');
-				   		  $("li").first().hide();
-				    }
+         			$(".errors").hide();
+         	}
+			if (res == true && res_pswd != true){
+				   	alert('Invalid password');
+				   	$("li").first().hide();
+			}
             if (res != true && res_pswd == true){
-                alert('Invalid email');
-                $('li').hide();
-                $('li:first-child').show();
+                    alert('Invalid email');
+                    $('li').hide();
+                    ('li:first-child').show();
             }
             if (res != true && res_pswd != true){
-                alert("Invalid email and password")
-                $(".errors").show();
+                    alert("Invalid email and password")
+                    $(".errors").show();
             }
   
       });
